1a152f4879a4d75c5e72d2b695287b71623bc9eb
Upmanyu Shrivastava <upmanyushrivastava98@gmail.com>
2021-01-18 23:32:40 +0530

    Deploy to master

--- _next/static/fITdffpMu11GGy6Vbh51F/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-0ea8a3e93ccc0303440c.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-f4551e24bfd0494c6f7c.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
